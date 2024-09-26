6364d2b89d8314a7f75a419f5184465201cb45f8
Mikko Keinänen <mikko.keinanen@gmail.com>
2024-09-26 23:05:24 +0300

    Initial ui with header, sidebar and content

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+use iced::widget::{button, column, container, horizontal_space, row, text};
+use iced::{Center, Element};
+pub fn main() -> iced::Result {
+    iced::run(
+        "Game Collection",
+        GameCollection::update,
+        GameCollection::view,
+    )
+}
+
+#[derive(Default)]
+struct GameCollection {
+    value: i32,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    Increment,
+    Decrement,
+}
+
+impl GameCollection {
+    fn update(&mut self, message: Message) {}
+
+    fn view(&self) -> Element<Message> {
+        let header = container(
+            row![horizontal_space(), "Game Collection", horizontal_space(),]
+                .padding(10)
+                .align_y(Center),
+        );
+        let sidebar = container(
+            column![
+                button("Home"),
+                button("Games"),
+                button("Settings"),
+                button("About"),
+            ]
+            .padding(10),
+        );
+        let content = container(
+            column![
+                text("Welcome to Game Collection!").size(50),
+                text("This is a collection of games that you can play.").size(20),
+            ]
+            .padding(20),
+        );
+        column![header, row![sidebar, content]].into()
+    }
 }
