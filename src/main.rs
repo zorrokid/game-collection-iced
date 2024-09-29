49f44788cfb24507fd0c771a7e96f5539c08055b
Mikko Keinänen <mikko.keinanen@gmail.com>
2024-09-29 12:04:13 +0300

    change content based on user action

--- src/main.rs
+++ src/main.rs
@@ -36,18 +36,23 @@ impl GameCollection {
     fn update(&mut self, message: Message) {
         match message {
             Message::AddGame => {
+                self.main_content = MainContent::AddGame;
                 println!("Add game");
             }
             Message::Games => {
+                self.main_content = MainContent::Games;
                 println!("Games");
             }
             Message::Home => {
+                self.main_content = MainContent::Home;
                 println!("Home");
             }
             Message::Settings => {
+                self.main_content = MainContent::Settings;
                 println!("Settings");
             }
             Message::About => {
+                self.main_content = MainContent::About;
                 println!("About");
             }
         }
@@ -70,13 +75,36 @@ impl GameCollection {
             .padding(10),
         );
 
-        let content = container(
-            column![
-                text("Welcome to Game Collection!").size(50),
-                text("This is a collection of games that you can play.").size(20),
-            ]
-            .padding(20),
-        );
+        let content = match self.main_content {
+            MainContent::Home => home_content(),
+            MainContent::AddGame => add_game_content(),
+            MainContent::Games => games_content(),
+            MainContent::Settings => settings_content(),
+            MainContent::About => about_content(),
+        };
+
         column![header, row![sidebar, content]].into()
     }
+}
+
+fn home_content<'a>() -> Element<'a, Message> {
+    column![
+        text("Welcome to Game Collection!").size(50),
+        text("This is a collection of games that you can play.").size(20),
+    ]
+    .padding(20)
+    .into()
+}
+
+fn add_game_content<'a>() -> Element<'a, Message> {
+    column![text("Add game").size(50),].padding(20).into()
+}
+fn games_content<'a>() -> Element<'a, Message> {
+    column![text("games").size(50),].padding(20).into()
+}
+fn settings_content<'a>() -> Element<'a, Message> {
+    column![text("Settings").size(50),].padding(20).into()
+}
+fn about_content<'a>() -> Element<'a, Message> {
+    column![text("About").size(50),].padding(20).into()
 }
